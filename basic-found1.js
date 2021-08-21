a1a204d5a941925056f96ce3a7f7cc81b27d1d3b
Joseph Hooker <jcolehooker@gmail.com>
2021-08-21 16:01:51 -0500

    completed problems 10-13; removed small unnecessary portions of earlier problems; commented out some console.logs that exist just for testing

--- basic-found1.js
+++ basic-found1.js
@@ -9,8 +9,8 @@ function upTo255() {
     return upToArray;
 }
 
-var a = upTo255();
-console.log(a);
+// var a = upTo255();
+// console.log(a);
 
 
 //2. Get even 1000 (function)
@@ -24,8 +24,8 @@ function getEvens() {
     return sumOf;
 }
 
-var b = getEvens();
-console.log(b);
+// var b = getEvens();
+// console.log(b);
 
 //3. Sum odd 5000 - fxn returns sum of odd numbers from 1 to 5000
 
@@ -39,7 +39,7 @@ function sumOdds() {
     return sumOf;
 }
 
-console.log(sumOdds());
+// console.log(sumOdds());
 
 //4. Iterate an array - sum of array
 
@@ -51,8 +51,8 @@ function sumOfArray(iterArray) {
     return sumOf;
 }
 
-var c = [-1, 44, 3, -20, 0, 12];
-console.log(sumOfArray(c));
+// var c = [-1, 44, 3, -20, 0, 12];
+// console.log(sumOfArray(c));
 
 //5. Find max
 
@@ -66,8 +66,8 @@ function findMax(iterArray) {
     return newMax;
 }
 
-var d = [333, -44, 0, 3333, 22, -200, 2121212, 1];
-console.log(findMax(d));
+// var d = [333, -44, 0, 3333, 22, -200, 2121212, 1];
+// console.log(findMax(d));
 
 //6. Find average
 
@@ -80,8 +80,8 @@ function avg(iterArray) {
     return runningSum / iterArray.length;
 }
 
-var e = [22, -55, 12, 100, 1, -2];
-console.log(avg(e));
+// var e = [22, -55, 12, 100, 1, -2];
+// console.log(avg(e));
 
 //7. Array odd - all odds between 1 and 50
 
@@ -93,7 +93,7 @@ function arrayOdd() {
     return oddArray;
 }
 
-console.log(arrayOdd());
+// console.log(arrayOdd());
 
 //8. Greater than Y
 
@@ -107,9 +107,9 @@ function greaterThanCount(testVal, testArray) {
     return amtGreaterThan;
 }
 
-var f = 19;
-var g = [-2, -33, 2000, 20, 18, -1500, 0, 22, 555];
-console.log(greaterThanCount(f,g));
+// var f = 19;
+// var g = [-2, -33, 2000, 20, 18, -1500, 0, 22, 555];
+// console.log(greaterThanCount(f,g));
 
 //9. Squares
 
@@ -120,5 +120,67 @@ function squareArray(iterArray) {
     return iterArray;
 }
 
-var h = [3, 22, -5, 0, 1, -4, 55];
-console.log(squareArray(h));
+// var h = [3, 22, -5, 0, 1, -4, 55];
+// console.log(squareArray(h));
+
+//10. Negatives
+
+function negToZero(iterArray) {
+    for (var i = 0; i < iterArray.length; i++) {
+        if (iterArray[i] < 0 ) {
+            iterArray[i] = 0;
+        }
+    }
+    return iterArray;
+}
+            
+// var k = [-3, 44, 1, 0, -58, -200, 101, -5];
+// console.log(negToZero(k));
+
+//11. Max/Min/Avg - uses functions from previous problems (#5 & #6), with a function explicitly added for finding minimum values
+
+function findMin(iterArray) {
+    var newMin = iterArray[0];
+    for (var i = 1; i < iterArray.length; i++) {
+        if (iterArray[i] < newMin) {
+            newMin = iterArray[i];
+        }
+    }
+    return newMin;
+}
+
+function maxMinAvg(iterArray) {
+    var newArray = [];
+    newArray.push(findMax(iterArray));
+    newArray.push(findMin(iterArray));
+    newArray.push(avg(iterArray));
+    return newArray;
+}
+
+// var l = [2, 0, -44, 12, -9, 304, -3];
+// console.log(maxMinAvg(l));
+
+//12. Swap values
+
+function swapEnds(arr) {
+    var temp = arr[arr.length - 1];
+    arr.pop();
+    arr.push(arr[0]);
+    arr[0] = temp;
+    return arr;
+}
+
+// console.log(swapEnds([500,-3]));
+
+//13. Number to string
+
+function numToString(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if(arr[i] < 0) {
+            arr[i] = "Dojo";
+        }
+    }
+    return arr;
+}
+
+// console.log(numToString([-2, 0, 1, 33, -200, -6, 5]));
